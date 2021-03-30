dd0df12e3f1f09d866469764616a883959023d22
josemamunoz <josema.munoz@gmail.com>
2021-03-30 09:24:05 -0300

    firebase

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -30,9 +30,9 @@ export const Home = () => {
               <div className="card" key={movie.imdbID}>
                 <img className="card-img " src={`${movie.Poster}`} alt="Card image cap" />
                 <div className="card-body ">
-                  <h5 className="card-title m-1">{movie.Title}</h5>
-                  <p className="card-text m-1">{movie.Type}</p>
-                  <p className="card-text m-1">{movie.Year}</p>
+                  <h5 className="card-title">{movie.Title}</h5>
+                  <p className="card-text">{movie.Type}</p>
+                  <p className="card-text">{movie.Year}</p>
                   <button className="boton-agregar" onClick={(e)=> actions.getCarrito(movie, e) } key={movie.imdbID}>Añadir al carrito</button>
                 </div>
               </div>
